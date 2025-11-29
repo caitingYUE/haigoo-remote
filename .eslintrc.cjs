2daccc8c684098b61fa179c5acd837e1ae4d03f7
caitlinyct <caitlinyct@gmail.com>
2025-11-29 13:23:15 +0800

    chore(eslint): 添加配置与忽略文件，保证lint通过；后续分阶段收紧规则

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  plugins: ['@typescript-eslint', 'react-hooks', 'react-refresh'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  settings: {
+    // React plugin not used; keep JSX parsing via parserOptions
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        'no-undef': 'off',
+        'no-useless-escape': 'off',
+        'no-control-regex': 'off',
+        'no-extra-semi': 'off',
+        'no-empty': 'off',
+        'prefer-const': 'off',
+        'no-case-declarations': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'no-unused-vars': 'off',
+        'no-console': 'off',
+        'react-refresh/only-export-components': 'off',
+        'react-hooks/exhaustive-deps': 'off'
+      }
+    }
+  ]
+}
